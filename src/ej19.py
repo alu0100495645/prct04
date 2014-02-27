48119e4ed6fdcf25d471385e32c43dd3b3e9a1f8
Alba Tome <alu0100495645@ull.edu.es>
2014-02-27 14:47:49 +0000

    Comentario del cambio

---
+++ src/ej19.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#!encoding: UTF-8
+
+a = float(raw_input('Valor de a: '))
+b = float(raw_input('Valor de b: '))
+a * x + b = 0
+print 'Solucion: ', x
+
+#No funciona, me sale lo siguiente al no asignar un operador :SyntaxError: can't assign to operator#
---
+++ src/ej20.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#!encoding: UTF-8
+
+x = -b/a
+a = float(raw_input('Valor de a: '))
+b = float(raw_input('Valor de b: '))
+
+print 'Solucion: ', x
+
+#No funciona porque no esta definida la variable b :NameError: name 'b' is not defined#
